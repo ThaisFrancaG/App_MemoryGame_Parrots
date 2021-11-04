7c48670fd76f9b2ae0c95c68c99c0ca96921dadd
Thais Franca <tfrancag@gmail.com>
2021-11-04 10:47:11 -0300

    agora tá dando pra virar as cartas, mas eu fiz as coisas meio fora de ordem.... Agora, preciso criar as cartas no começo do jogo, e atribuir os valores de verso

--- operacao.js
+++ operacao.js
@@ -1,11 +1,24 @@
-function virarCarta(carta) {
 
-    if (carta !== null){
-        alert("estou funcionando!");
+let parCartas = [];
+let ii=0;
+
+
+function virarCarta(carta) {
 carta.classList.toggle("frente");
 carta.classList.toggle("verso");
-carta.innerHTML.src = "imagens/IMG_2853.JPEG";
-    }
+carta.innerHTML = `<figure><img src = 'imagens/verso${ii+1}.JPEG' /></figure>`;
+parCartas.push(carta);
+console.log(ii);
+ii++;
+
+if (parCartas.length>2 && parCartas[0] !== parCartas[1]){
+    parCartas[0].classList.toggle("frente");
+    parCartas[0].classList.toggle("verso");
+    parCartas[0].innerHTML = `<figure><img src = 'imagens/front.png' /></figure>`;
+    parCartas[1].classList.toggle("frente");
+    parCartas[1].classList.toggle("verso");
+    parCartas[1].innerHTML = `<figure><img src = 'imagens/front.png' /></figure>`;
+}
 
 
 }
