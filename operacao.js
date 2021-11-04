81630e9025aecfc43197d13d2a922bc85ded5b03
Thais Franca <tfrancag@gmail.com>
2021-11-04 11:54:44 -0300

    agora as cartas estão sendo criadas, com os versos aleatórios. Só bolada que peguei a funçào para embaralhar direto do notion, depois tento fazer uma pra mim

--- operacao.js
+++ operacao.js
@@ -1,6 +1,54 @@
+//primeiro, com quantas cartas será o jogo
+// numeroCartas = prompt('Insira um número par de 4 a 14!');
 
-let parCartas = [];
-let ii=0;
+// while (numeroCartas<4 || numeroCartas>14 || numeroCartas%2 !==0) {
+//     alert('Desculpe, mas você inseriu um número invalido!');
+//     numeroCartas = prompt('Insira um número par de 4 a 14!');
+// }
+
+numeroCartas = 4;
+
+//pegar as opcoes possiveis de verso para construir as cartas
+let numeroDeVersos = [];
+let numeroVerso = [];
+
+for (let i=0;i<(numeroCartas/2);i++){
+    numeroDeVersos.push(i+1);
+    let numeroVerso = numeroDeVersos.concat(numeroDeVersos);
+    console.log(numeroDeVersos);
+    console.log(numeroVerso);
+    numeroVerso.sort(comparador);
+//peguei do notion, depois quero testar outros jeitos de fazer
+    function comparador(){
+        return Math.random() - 0.5; 
+    }
+    console.log(numeroVerso+" "+"embaralhado");
+}
+
+//Depois, criar as cartas que serão usadas. Isso deve ser feito no JS e no HTML
+
+let contadorCartas = 0;
+
+while (contadorCartas<numeroCartas){
+
+    let mesa = document.querySelector(".mesa-cartas");
+    mesa.innerHTML +=` <div class = "carta" onclick = "virarCarta(this)">
+    <figure class = "frente">
+         <img src="imagens/front.png"/>
+     </figure>
+     <figure class = "verso">
+         <img src="imagens/verso${1}.JPEG"/>
+     </figure>
+        </div>`;
+        contadorCartas++                                     
+}
+
+
+
+
+
+// let parCartas = [];
+// let ii=0;
 
 
 function virarCarta(carta) {
